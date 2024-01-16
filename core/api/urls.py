016c76affc14ec520b88661e7991cf4f74d51ca3
Artem <vebulogmetra@yandex.ru>
2024-01-16 18:59:31 +0300

    add root routing

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,1 +1,21 @@
-urlpatterns = []
+from django.http import HttpRequest
+from django.urls import path
+
+from ninja import NinjaAPI
+
+from core.api.schemas import PingResponseSchema
+from core.api.v1.urls import router as v1_router
+
+api = NinjaAPI()
+
+api.add_router("v1/", v1_router)
+
+
+@api.get("/ping", response=PingResponseSchema)
+def ping(request: HttpRequest):
+    return PingResponseSchema(result=True)
+
+
+urlpatterns = [
+    path("", api.urls),
+]
