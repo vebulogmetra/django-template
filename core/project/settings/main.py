bf9b3f90defe24fe66abb54929e21bc4c5855f9e
Artem <vebulogmetra@yandex.ru>
2024-01-16 19:00:01 +0300

    include products app

--- core/project/settings/main.py
+++ core/project/settings/main.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # first party
+    "core.apps.products.apps.ProductsConfig",
 ]
 
 MIDDLEWARE = [
